90da3b184336a8ca33253d257c500c293fb6eb9e
Sefa Ertunç <sefaertnc@gmail.com>
2024-06-01 12:00:24 +0200

    EDIT: Files

---
+++ Data_Main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import glob
+import os
+
+folder_path = 'data/ScrewDriver/ScrewDriverClean'
+
+csv_files = glob.glob(os.path.join(folder_path, '*.csv'))
+
+row_counts = []
+
+dataframes = []
+for file in csv_files:
+    df = pd.read_csv(file)
+    dataframes.append(df)
+    row_counts.append(len(df))
+
+min_rows = min(row_counts)
+
+for i, file in enumerate(csv_files):
+    df = dataframes[i]
+    truncated_df = df.iloc[:min_rows]
+    truncated_df.to_csv(file, index=False)
+
